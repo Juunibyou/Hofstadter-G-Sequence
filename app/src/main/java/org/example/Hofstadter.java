65ae35785a1f8f0ab001ebce061f15d563255455
Juunibyou <179892743+Juunibyou@users.noreply.github.com>
2025-09-13 15:46:13 -0500

    update

--- app/src/main/java/org/example/Hofstadter.java
+++ app/src/main/java/org/example/Hofstadter.java
@@ -1,11 +1,28 @@
 package org.example;
 
 public class Hofstadter {
-  // Your implementations of the sequence goes here.
   // Recall that you can calculate the nth term, G(n), using:
   // G(n) = n - G(G(n-1))
   // G(0) = 0
-  public Integer gSequence(Integer index) {
-    return 0;
+
+  public int gNaive(int n) {
+    if (n == 0){
+      return 0;
+    } else {
+      return n - gNaive(n - 1);
+    }
+  }
+
+  public int gMemorized(int n, int[] arr){
+    if (n == 0){
+      return 0;
+    }
+    if (arr[n] != -1){
+      return arr[n];
+    } else {
+      arr[n] = n - gMemorized(gMemorized(n - 1, arr), arr);
+      return arr[n];
+    }
   }
 }
+
